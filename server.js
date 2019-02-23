a21627e424b89b30267ab12e399b6b7c25d3eff3
junjo255 <jun.jo249@gmail.com>
2019-02-23 10:33:48 -0500

    icon add

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const parser = require('body-parser') //instantiate an express object by calling it. 
 const path = require('path');
 const app = express();
+const cors = require('cors')
+
+app.use(cors()) 
 
 app.use(parser.json());
 app.use(express.urlencoded({ extended: true }))
